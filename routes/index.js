a4e87a50c53d81cda0845420a266576d649c3d36
thedoru <thedoruv2@gmail.com>
2022-12-02 16:37:15 +0100

    Project's routers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    date: new Date(),
+  },
+  {
+    text: "Hello world!",
+    user: "Charles",
+    date: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
+});
+
+router.get("/new", (req, res, next) => {
+  res.render("form", { title: "Mini Messageboard" });
+});
+
+router.post("/new", (req, res, next) => {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    date: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
